0c80c9c2f51ce9c84ffd196d0155f25cda5fe22d
Choi.SY <sollly0829@gmail.com>
2021-08-03 13:17:58 +0900

    Ex06 String 기본

---
+++ src/day08_set_map_string/Ex06_String.java
@@ -0,0 +1,18 @@
+package day08_set_map_string;
+
+import java.util.Scanner;
+
+public class Ex06_String {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		//String str;
+		String str = new String();
+		
+		System.out.print("문자열 입력 : ");
+		str=sc.next();
+		System.out.println("입력받은 문자열 : " + str);
+	}
+}
