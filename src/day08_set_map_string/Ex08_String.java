4493274bfd8e713e736287511f47003c97a36de5
Choi.SY <sollly0829@gmail.com>
2021-08-03 13:26:09 +0900

    Ex08_String string length, charAt

---
+++ src/day08_set_map_string/Ex08_String.java
@@ -0,0 +1,23 @@
+package day08_set_map_string;
+
+public class Ex08_String {
+	
+	public static void main(String[] args) {
+		
+		String str = new String("abcdefg");
+		
+		System.out.println(str.length());
+		System.out.println(str.charAt(2));
+		
+		str+="æ»≥Á«œººø‰";
+		System.out.println(str);
+		
+		System.out.println(str.equals("abcdefgæ»≥Á«œººø‰"));
+		
+		System.out.println(str.charAt(0) == 'a');
+		System.out.println(str.charAt(0) == 'd');
+		
+		System.out.println(str.charAt(0)-32);
+		System.out.println((char)(str.charAt(0)-32));
+	}
+}
